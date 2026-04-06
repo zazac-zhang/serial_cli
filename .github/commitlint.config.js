0688133ad041f682b09c01e15cb32d2c378e3a42
pony <trans-bug@outlook.com>
2026-04-06 11:25:29 +0800

    chore(ci): add commitlint configuration

---
+++ .github/commitlint.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  extends: ['@commitlint/config-conventional'],
+  rules: {
+    'type-enum': [2, 'always', [
+      'feat', 'fix', 'docs', 'style', 'refactor',
+      'perf', 'test', 'chore', 'ci'
+    ]],
+    'scope-enum': [2, 'always', [
+      'cli', 'gui', 'serial', 'protocol', 'lua',
+      'task', 'config', 'build', 'ci'
+    ]],
+    'type-case': [2, 'always', 'lower-case'],
+    'subject-case': [2, 'always', 'sentence-case'],
+    'subject-empty': [2, 'never'],
+    'subject-full-stop': [2, 'never', '.'],
+    'header-max-length': [2, 'always', 100]
+  }
+};
